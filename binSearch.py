cbfe71af936053266ca97c1a1f413819b987ca36
Maitraya Kakade <47203424+mkakade92@users.noreply.github.com>
2020-03-19 23:47:40 +0530

    Add files via upload

---
+++ binSearch.py
@@ -0,0 +1,13 @@
+def binarySerch(array,key,lower_bound,upper_bound):
+    if(lower_bound>upper_bound):
+        return False
+    else:
+        middle=(lower_bound+upper_bound)//2
+        if(array[middle]==key):
+            return True
+        elif(array[middle]>key):
+            return binarySearch(array,key,lower_bound,middle)#[lower_bound,middle)
+        else:
+            return binarySearch(array,key,middel,upper_bound)
+
+
